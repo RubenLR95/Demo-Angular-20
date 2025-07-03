ee3258c6b70e711bfcead222bbe14a6607522a16
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-07-03 12:44:08 +0200

    estructura

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import MyPreset from '../themes/mypreset';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+     provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset:  MyPreset
+            }
+        })
   ]
 };
---
+++ src/app/shared/pipes/capitalize/capitalize.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capitalize',
+  imports: [],
+  templateUrl: './capitalize.html',
+  styleUrl: './capitalize.css'
+})
+export class Capitalize {
+
+}
